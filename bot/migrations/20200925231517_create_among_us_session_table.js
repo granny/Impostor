932fa052dbb4b9e65b459012429e7eca804318bb
aPinat <apinat@apinat.de>
2020-10-18 14:16:42 +0200

    use proper specific type for 'id'

--- bot/migrations/20200925231517_create_among_us_session_table.js
+++ bot/migrations/20200925231517_create_among_us_session_table.js
@@ -1,6 +1,6 @@
 exports.up = knex =>
     knex.schema.createTable("among_us_session", table => {
-        table.increments("id").primary();
+        table.specificType("id", "integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY");
         table.specificType("guild", "text").notNullable();
         table.specificType("channel", "text").notNullable();
         table.specificType("message", "text").notNullable();
